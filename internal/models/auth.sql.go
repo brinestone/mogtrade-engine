41aaffb7d27eeb43676aef937c1042ce126c51d6
Conrad Bekondo <conradbekondo@gmail.com>
2026-08-30 20:17:46 +0100

    refactor: output models directory

--- internal/models/auth.sql.go
+++ internal/models/auth.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.31.1
 // source: auth.sql
 
-package models
+package db
 
 import (
 	"context"
--- internal/models/db.go
+++ internal/models/db.go
@@ -2,7 +2,7 @@
 // versions:
 //   sqlc v1.31.1
 
-package models
+package db
 
 import (
 	"context"
