9576b7962aa3b4a27d0aca27a3fee206a3ccc257
Conrad Bekondo <conradbekondo@gmail.com>
2026-08-30 12:53:15 +0100

    feat: setup gin framework

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
-	
-	env "github.com/joho/godotenv"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/brinestone/mogtrade/libs/api"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	port int
 )
 
 func main() {
-	env.Load()
+	err := parseVars()
+	if err != nil {
+		panic(err)
+	}
+	engine := gin.Default()
+	baseRouter := engine.Group("/api")
+	api.MountApiV1(baseRouter)
+	engine.Run(fmt.Sprintf(":%d", port))
+}
+
+func parseVars() error {
+	_port, err := strconv.ParseInt(os.Getenv("PORT"), 10, 32)
+	if err != nil {
+		return err
+	}
+	port = int(_port)
+	return nil
 }
---
+++ libs/api/api.go
@@ -0,0 +1,6 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+func MountApiV1(r *gin.RouterGroup) {
+}
