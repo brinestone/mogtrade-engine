b7fc3f80b979d3238d1bdce680eb3a8a77e9ae30
Conrad Bekondo <conradbekondo@gmail.com>
2026-09-09 12:14:24 +0100

    fix(api): allow refresh token header in cors

--- app/api/api.go
+++ app/api/api.go
@@ -63,7 +63,7 @@ func MountGlobalMiddlewares(e *gin.Engine, origins []string) {
 	e.Use(cors.New(cors.Config{
 		AllowOrigins:     origins,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "X-D-Id"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "X-D-Id", "X-Refresh-Token"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
